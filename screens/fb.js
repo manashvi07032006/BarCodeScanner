e74bff7eebdade454db090d353db9badaf5a0a16
manashvi07032006 <80456520+manashvi07032006@users.noreply.github.com>
2021-09-01 14:41:06 +0530

    Add files via upload

---
+++ screens/fb.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Facebook extends Component {
+  render(){
+  return (
+    <View>
+      <Text style={{alignItems:"center", textAlign:"center", fontSize:18, justifyContent:"center"}}>FACEBOOK</Text>
+    </View>
+  );
+  }
+}
---
+++ screens/in.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Instagram extends Component {
+  render(){
+  return (
+    <View>
+      <Text style={{alignItems:"center", textAlign:"center", fontSize:14, justifyContent:"center"}}>INSTAGRAM</Text>
+    </View>
+  );
+  }
+}
